b39b8ae34231ddb9e3d29c9d8669adfd3981beda
bobsin <anggaratyas15@gmail.com>
2019-12-18 18:54:19 +0700

    memperbaiki query tambah penduduk

--- resources/views/dashboard/admin/reg.blade.php
+++ resources/views/dashboard/admin/reg.blade.php
@@ -119,12 +119,12 @@
 
                         <div class="form-group">
                           <label>Nama Lengkap</label>
-                            <input name="nama_lengkap" type="name" class="form-control" placeholder="Nama Lengkap">                         
+                            <input name="nama_lengkap" type="name" class="form-control" placeholder="Nama Lengkap" value="{{old('nama_lengkap')}}">                         
                         </div>
 
                         <div class="form-group">
                           <label>Tempat Lahir</label>
-                            <input name="tempat_lahir" type="name" class="form-control" placeholder="Tempat Lahir">                         
+                            <input name="tempat_lahir" type="name" class="form-control" placeholder="Tempat Lahir" value="{{old('tempat_lahir')}}">                         
                         </div>
 
                         <div class="form-group">
@@ -134,14 +134,14 @@
                             <div class="input-group-addon">
                               <i class="fa fa-calendar"></i>
                             </div>
-                            <input name="tgl_lahir" type="text" class="form-control" id="datepicker" data-inputmask="'alias': 'mm/dd/yyyy'" data-mask>
+                            <input name="tgl_lahir" type="text" class="form-control" id="datepicker" data-inputmask="'alias': 'mm/dd/yyyy'" data-mask value="{{old('tgl_lahir')}}">
                           </div>
     
                         </div>
 
                         <div class="form-group">
                           <label>Agama</label>
-                          <select name="agama" class="form-control select2" style="width: 100%;">
+                          <select name="agama" class="form-control select2" style="width: 100%;" value="{{old('agama')}}">
                             <option>Islam</option>
                             <option>Kristen Protestan</option>
                             <option>Katolik</option>
@@ -153,7 +153,7 @@
 
                         <div class="form-group">
                           <label>Pendidikan</label>
-                          <select name="skul" class="form-control select2" style="width: 100%;">
+                          <select name="skul" class="form-control select2" style="width: 100%;" value="{{old('skul')}}">
                             <option>Tidak Sekolah</option>
                             <option>Sekolah Rakyat</option>
                             <option>SD/MI/Sederajat</option>
@@ -171,7 +171,7 @@
 
                         <div class="form-group">
                           <label>NO Telpon</label>
-                          <input name="tlp" type="text" class="form-control" data-inputmask='"mask": "99999999999999"' data-mask>                       
+                          <input name="tlp" type="text" class="form-control" data-inputmask='"mask": "99999999999999"' data-mask value="{{old('tlp')}}">                       
                         </div>
 
                       </div>
@@ -185,7 +185,7 @@
 
                         <div class="form-group">
                           <label>Jenis Kelamin</label>
-                          <select name="jenis_kel" class="form-control select2" style="width: 100%;">
+                          <select name="jenis_kel" class="form-control select2" style="width: 100%;" value="{{old('jenis_kel')}}">
                             <option>Laki-Laki</option>
                             <option>Perempuan</option>
                           </select>                       
@@ -193,27 +193,27 @@
 
                         <div class="form-group">
                           <label>NO Identitas</label>
-                          <input name="nik" type="text" class="form-control" data-inputmask='"mask": "9999999999999999"' data-mask>                       
+                          <input name="nik" type="text" class="form-control" data-inputmask='"mask": "9999999999999999"' data-mask value="{{old('nik')}}">                       
                         </div>
 
                         <div class="form-group">
                           <label>NO. Kartu Keluarga</label>
-                          <input name="kk" type="text" class="form-control" data-inputmask='"mask": "9999999999999999"' data-mask>                                                  
+                          <input name="kk" type="text" class="form-control" data-inputmask='"mask": "9999999999999999"' data-mask value="{{old('kk')}}">                                                  
                         </div>
 
                         <div class="form-group">
                           <label>Nama Ibu Kandung</label>
-                            <input name="ibu" type="name" class="form-control" placeholder="Nama Ibu">                         
+                            <input name="ibu" type="name" class="form-control" placeholder="Nama Ibu" value="{{old('ibu')}}">                         
                         </div>
 
                         <div class="form-group">
                           <label>Nama Ayah Kandung</label>
-                            <input name="ayah" type="name" class="form-control" placeholder="Nama Ayah">                         
+                            <input name="ayah" type="name" class="form-control" placeholder="Nama Ayah" value="{{old('ayah')}}">                         
                         </div>
 
                         <div class="form-group">
                           <label>Kewarganegaraan</label>
-                          <select name="kwn" class="form-control select2" style="width: 100%;">
+                          <select name="kwn" class="form-control select2" style="width: 100%;" value="{{old('kwn')}}">
                             <option>Warga Negara Indonesia (WNI)</option>
                             <option>Warga Negara Asing (WNA)</option>
                           </select>                      
@@ -240,32 +240,32 @@
 
                       <div class="form-group">
                         <label>Alamat Identitas</label>
-                          <input name="alamat" type="name" class="form-control" placeholder="Alamat Identitas">                         
+                          <input name="alamat" type="name" class="form-control" placeholder="Alamat Identitas" value="{{old('alamat')}}">                         
                       </div>
 
                       <div class="form-group">
                         <div class="row">
                           <div class="col-xs-3">
                             <label>RT</label>
-                            <input name="rt" type="text" class="form-control" data-inputmask='"mask": "999"' data-mask>
+                            <input name="rt" type="text" class="form-control" data-inputmask='"mask": "999"' data-mask value="{{old('rt')}}">
                           </div>
                           <div class="col-xs-3">
                             <label>RW</label>
-                            <input name="rw" type="text" class="form-control" data-inputmask='"mask": "999"' data-mask>
+                            <input name="rw" type="text" class="form-control" data-inputmask='"mask": "999"' data-mask value="{{old('rw')}}">
                           </div>
                         </div>                  
                       </div>
 
                       <div class="form-group">
                         <label>Propinsi</label>
-                          <select name="propinsi" class="form-control select2" style="width: 100%;">
+                          <select name="propinsi" class="form-control select2" style="width: 100%;" value="{{old('propinsi')}}">
                             <option>-- Silahkan Pilih Propinsi --</option>
                             <option>Data Base</option>
                           </select>                          
                       </div>
                       <div class="form-group">
                         <label>Kabupaten/Kota</label>
-                          <select name="kabupaten" class="form-control select2" style="width: 100%;">
+                          <select name="kabupaten" class="form-control select2" style="width: 100%;" value="{{old('kabupaten')}}">
                             <option>-- Silahkan Pilih Kabupaten --</option>
                             <option>Data Base</option>
                           </select>                         
@@ -273,7 +273,7 @@
 
                       <div class="form-group">
                         <label>Kecamatan</label>
-                        <select name="kecamatan" class="form-control select2" style="width: 100%;">
+                        <select name="kecamatan" class="form-control select2" style="width: 100%;" value="{{old('kecamatan')}}">
                             <option>-- Silahkan Pilih Kecamatan --</option>
                             <option>Data Base</option>
                           </select>                      
@@ -281,7 +281,7 @@
 
                       <div class="form-group">
                         <label>Desa</label>
-                        <select name="desa" class="form-control select2" style="width: 100%;">
+                        <select name="desa" class="form-control select2" style="width: 100%;" value="{{old('desa')}}">
                             <option>-- Silahkan Pilih Desa --</option>
                             <option>Data Base</option>
                           </select>                       
@@ -289,12 +289,12 @@
 
                       <div class="form-group">
                         <label>KodePos</label>
-                        <input name="kdpos" type="text" class="form-control" data-inputmask='"mask": "9999999"' data-mask>                        
+                        <input name="kdpos" type="text" class="form-control" data-inputmask='"mask": "9999999"' data-mask value="{{old('kdpos')}}">                        
                       </div>
 
                       <div class="form-group">
                         <label>Email</label>
-                          <input name="email" type="email" class="form-control" placeholder="Email">                         
+                          <input name="email" type="email" class="form-control" placeholder="Email" value="{{old('email')}}">                         
                       </div>
                     </div>
                     <!-- /.box-body -->
